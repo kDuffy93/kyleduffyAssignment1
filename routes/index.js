e9349afa7c8bc0df93e9ad823babfbb5820b39af
KyleDuffy93 <kyleduffy_83@Hotmail.com>
2017-02-16 07:33:05 -0500

    had to re upload for proper url

---
+++ routes/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('home', { title: 'Express' });
+});
+<!--  GET about page  -->
+router.get('/about', function(req, res, next) {
+    res.render('about', { title: 'Express' });
+});
+<!-- GET contact page   -->
+router.get('/contact', function(req, res, next) {
+    res.render('contact', { title: 'Express' });
+});
+<!--  GET  projects page  -->
+router.get('/projects', function(req, res, next) {
+    res.render('projects', { title: 'Express' });
+});
+<!-- GET services page   -->
+router.get('/services', function(req, res, next) {
+    res.render('services', { title: 'Express' });
+});
+
+module.exports = router;
